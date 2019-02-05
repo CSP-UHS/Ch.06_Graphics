43437c6633bfd0330270f8503867f20f5ec1e518
randallale <randallale@urbandaleschools.com>
2019-02-05 12:14:34 -0600

    A

--- TestFile.py
+++ TestFile.py
@@ -1,2 +1,10 @@
 import arcade
-arcade.open_window(600,600, "Star Wars Art")
+arcade.open_window(500,500,"Andre")
+arcade.set_background_color((0,0,90))
+
+arcade.start_render()
+arcade.draw_rectangle_filled(250, 250, 50, 50, arcade.color.CARMINE_PINK)
+
+arcade.finish_render()
+
+arcade.run()
