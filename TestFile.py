5d09e6be1af62f84091ec87874700bceed99bba9
randallale <randallale@urbandaleschools.com>
2019-02-05 12:18:00 -0600

    A

--- TestFile.py
+++ TestFile.py
@@ -4,6 +4,7 @@ arcade.set_background_color((0,0,90))
 
 arcade.start_render()
 arcade.draw_rectangle_filled(250, 250, 50, 50, arcade.color.CARMINE_PINK)
+arcade.draw_text("The tramp",250,400,arcade.color.RED,20)
 
 arcade.finish_render()
 
