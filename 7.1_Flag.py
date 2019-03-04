99e3f85cce0a3843785038c96d3fc0131482193d
OrigamiElephant <mcculleyezr@urbandaleschools.com>
2019-03-04 12:14:55 -0600

    Finished 7.1 Flag

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -14,10 +14,10 @@ arcade.open_window(494, 260, "The Stars and Stripes")
 arcade.set_background_color((255,255,255))
 arcade.start_render()
 for y_offset in range(10, 500, 40):
-    arcade.draw_rectangle_filled(0, 0 + y_offset, 20, 988, (255, 0, 0), 90)
-arcade.draw_lrtb_rectangle_filled(0, 218, 260, 120, (00, 85, 155))
-for stars in range(218, 120, 15):
-    arcade.draw_text("*  *  *  *  *  *", 10, 233, (255, 255, 255), 28)
-    arcade.draw_text("*  *  *  *  *", 27, 217, (255, 255, 255), 28)
+    arcade.draw_rectangle_filled(0, 0 + y_offset, 20, 988, (255, 0, 0), 90)  # Red stripes
+arcade.draw_lrtb_rectangle_filled(0, 218, 260, 120, (00, 85, 155))  # Blue rectangle
+for stars in range(0, 140, 31):
+    arcade.draw_text("*  *  *  *  *  *", 10, 231 - stars, (255, 255, 255), 28)  # Stars
+    arcade.draw_text("*  *  *  *  *", 27, 216 - stars, (255, 255, 255), 28)
 arcade.finish_render()
 arcade.run()
