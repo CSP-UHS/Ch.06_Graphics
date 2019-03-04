130645ffa8acd59eef4cb7cde44df3a0f22c7289
randallale <randallale@urbandaleschools.com>
2019-03-04 12:06:31 -0600

    Hahaha

--- 7.2_Picasso.py
+++ 7.2_Picasso.py
@@ -15,16 +15,24 @@ After you have showed your picture to your instructor, screenshot your picture,
 name it firstname_lastname.jpg and use the submit button to e-mail it to your instructor
 '''
 import arcade,random
+circle_x = 0
 arcade.open_window(500,500,"Picasso Project") #Creates the window
-arcade.set_background_color(arcade.color.SKY_BLUE)
+arcade.set_background_color(arcade.color.SKY_BLUE) #Makes background color sky color
 arcade.start_render()
 arcade.draw_circle_filled(300,400,100,arcade.color.SUNGLOW)
-for i in range(6):
+
+for i in range(6): #Generates Clouds
     y = random.randrange(200, 450)
     x = random.randrange(1, 501)
-    for i in range(7):
+    for i in range(random.randrange(4,8)):
         arcade.draw_circle_filled(x,y,30,arcade.color.LIGHT_GRAY)
         x += 30
+
+for i in range(1): #Generates the grass
+    for i in range(25):
+        arcade.draw_circle_filled(circle_x,25,25,arcade.color.GO_GREEN)
+        circle_x += 25
+
 arcade.draw_rectangle_filled(250,0,500,50,arcade.color.GO_GREEN)
 arcade.finish_render()
 arcade.run()
