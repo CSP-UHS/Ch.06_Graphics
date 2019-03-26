21adac7899f8f17515273f310e9e75798092d0b3
OrigamiElephant <mcculleyezr@urbandaleschools.com>
2019-03-26 11:55:58 -0500

    Finished take home test

--- 7.0_Take_Home_Test.py
+++ 7.0_Take_Home_Test.py
@@ -1,6 +1,6 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: ______________________
+but I will definitely not ask another person except the instructor. Signed: Ezra McCulley
 
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
@@ -26,6 +26,14 @@ arcade.draw_circle_filled(250, 200, 41, arcade.color.WISTERIA)  # Wisteria Circl
 arcade.draw_ellipse_filled(100, 99, 60, 19.5, arcade.color.AMBER)  # Amber Ellipse
 
 arcade.draw_rectangle_filled(460, 10, 5, 5, arcade.color.RED)  # Red Square
+
+arcade.draw_rectangle_filled(99, 40, 59.5, 1, arcade.color.BLUE, 45)  # Blue Line
+
+arcade.draw_text("I love you. I know.", 19, 159, arcade.color.BRICK_RED, 20)  # Brick Letters
+
+arcade.draw_arc_filled(400, 320, 60, 60, arcade.color.YELLOW, 0, 300, 30)
+
 arcade.finish_render()
+
 
 arcade.run()
