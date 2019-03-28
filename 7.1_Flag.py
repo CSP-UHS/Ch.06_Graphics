6021f98b9722ec18d718ce7eb30bb12f4c943f13
noahmarble <mablenoa@urbandaleschools.com>
2019-03-28 12:25:27 -0500

    star

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -12,9 +12,18 @@ The record is 16! You will have to use some loops to achieve this.
 import arcade
 arcade.open_window(494,260,"flag")
 arcade.set_background_color((255, 255, 255))
-
+redy =0
+stary=0
 arcade.start_render()
-arcade.draw_line(75, 590, 95, 570, arcade.color.BLACK, 2)
-arcade.draw_lrtb_rectangle_filled(0, 197.6, 260, 120, (0,0,128))
+for i in range(7):                                                               #red lines
+    arcade.draw_lrtb_rectangle_filled(0, 494, 20+redy, 0+redy, (200,0,0))
+    redy+= 40
+arcade.draw_lrtb_rectangle_filled(0, 197.6, 260, 120, (0,0,128))                #blue rectangle
+for i in range(1,10):                                                           #stars
+    if i%2==0:
+        arcade.draw_text("*   *   *   *   *   *", 8.822, 237.952-stary, (255,255,255) , 20.246)
+    else:
+        arcade.draw_text("*   *   *   *   *", 24.752, 223.912-stary, (255, 255, 255), 20.246)
+    stary+=28.08
 arcade.finish_render()
 arcade.run()
