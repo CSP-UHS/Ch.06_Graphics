613fc73dddbaf28707b74d186fe56de90dc999e5
noahmarble <mablenoa@urbandaleschools.com>
2019-04-02 12:19:17 -0500

    working on stars

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -10,9 +10,8 @@ for i in range(7):                                                              
 arcade.draw_lrtb_rectangle_filled(0, 197.6, 260, 120, (0, 0, 128))           #blue rectangle
 for i in range(5): #stars
     arcade.draw_text("*   *   *   *   *   *", 8.822, 233 - stary, (255, 255, 255), 20.246)
-    stary += 14.04
-    arcade.draw_text("*   *   *   *   *", 24.752, 233 - stary, (255, 255, 255), 20.246)
-    stary += 14.04
+    arcade.draw_text("*   *   *   *   *", 24.752, 233-14.04 - stary, (255, 255, 255), 20.246)
+    stary += 14.04*2
 arcade.finish_render()
 arcade.run()
 
