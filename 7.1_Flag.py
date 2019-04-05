7776b9e58734b165510985d0a8a51414e96b6be2
agicedd <agicedd@urbandaleschools.com>
2019-04-05 12:22:40 -0500

    flag done!

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -1,21 +1,13 @@
-'''
-FLAG PROJECT
----------------
-Make your flag 260 pixels tall
-Use the scaling image on the website to determine other dimensions
-The hexadecimal colors for the official flag are red:#BF0A30 and blue:#002868
-Title the window, "The Stars and Stripes"
-I used a draw_text command and used 20 pt. asterisks for the stars.
-We will have a competition to see who can make this flag in the least lines of code.
-The record is 16! You will have to use some loops to achieve this.
-'''
-
 import arcade
 arcade.open_window(494,260,"The Stars and Stripes")
 arcade.set_background_color(arcade.color.WHITE)
 arcade.start_render()
 for y_offset in range(0,260,40):
-    arcade.draw_rectangle_filled(247,10+y_offset,494,20,arcade.color.RED)
-arcade.draw_rectangle_filled(98.8,195,197.6,150,arcade.color.BLUE)
+    arcade.draw_rectangle_filled(247,10+y_offset,494,20,((191,10,48)))
+arcade.draw_rectangle_filled(98.8,195,197.6,150,((0,40,104)))
+for star_offset1 in range(0,130,28):
+    arcade.draw_text("*   *   *   *   *   *",16.38,231.92-star_offset1,arcade.color.WHITE,20)
+for star_offset2 in range(0,85,28):
+    arcade.draw_text("*   *   *   *   *",32.76,217.92-star_offset2,arcade.color.WHITE,20)
 arcade.finish_render()
 arcade.run()
