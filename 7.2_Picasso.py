d6b49617db2248e9ceb23f3050a0a13b41b87904
agicedd <agicedd@urbandaleschools.com>
2019-04-18 12:03:52 -0500

    F-I-N-I-S-H-E-D

--- 7.2_Picasso.py
+++ 7.2_Picasso.py
@@ -1,23 +1,6 @@
-'''
-PICASSO PROJECT
----------------
-Your job is to make a cool picture.
-You must use multiple colors.
-You must have a coherent picture. No abstract art with random shapes.
-You must use multiple types of graphic functions (e.g. circles, rectangles, lines, etc.)
-Somewhere you must include a WHILE or FOR loop to create a repeating pattern.
-Do not just redraw the same thing in the same location 10 times. 
-You can contain multiple drawing commands in a loop, so you can draw multiple train cars for example.
-Please use comments and blank lines to make it easy to follow your program. 
-If you have 5 lines that draw a robot, group them together with blank lines above and below. 
-Then add a comment at the top telling the reader what you are drawing.
-IN THE WINDOW TITLE PLEASE PUT YOUR NAME.
-When you are finished Pull Request your file to your instructor.
-'''
-
 import arcade
-arcade.open_window(425,585, "HELLO")
-arcade.set_background_color(arcade.color.GRANNY_SMITH_APPLE)
+arcade.open_window(425,585, "Eddie Agic")
+arcade.set_background_color((200,200,200))
 arcade.start_render()
 arcade.draw_rectangle_filled(107.5,55,205,100,arcade.color.WHITE)
 arcade.draw_rectangle_filled(265,55,100,100,arcade.color.WHITE)
@@ -29,6 +12,29 @@ for x_offset in range(55, 371, 105):
     arcade.draw_rectangle_filled(x_offset,475,100,100,arcade.color.WHITE)
     arcade.draw_rectangle_filled(x_offset,555,100,50,arcade.color.WHITE)
 arcade.draw_rectangle_filled(370,107.5,100,205,arcade.color.WHITE)
+
+arcade.draw_text("F16",75,535,arcade.color.GRAY,10)
+arcade.draw_text("F17",180,535,arcade.color.GRAY,10)
+arcade.draw_text("F18",285,535,arcade.color.GRAY,10)
+arcade.draw_text("F19",390,535,arcade.color.GRAY,10)
+arcade.draw_text("clear",33.3,470,arcade.color.GRAY,15)
+arcade.draw_text(".",259,45,arcade.color.GRAY,25)
+arcade.draw_text("=",150,465,arcade.color.GRAY,20)
+arcade.draw_text("/",260,465,arcade.color.GRAY,20)
+arcade.draw_text("*",362.5,455,arcade.color.GRAY,25)
+arcade.draw_text("7",45,360,arcade.color.GRAY,25)
+arcade.draw_text("8",150,360,arcade.color.GRAY,25)
+arcade.draw_text("9",255,360,arcade.color.GRAY,25)
+arcade.draw_text("4",45,255,arcade.color.GRAY,25)
+arcade.draw_text("5",150,255,arcade.color.GRAY,25)
+arcade.draw_text("6",255,255,arcade.color.GRAY,25)
+arcade.draw_text("1",45,150,arcade.color.GRAY,25)
+arcade.draw_text("2",150,150,arcade.color.GRAY,25)
+arcade.draw_text("3",255,150,arcade.color.GRAY,25)
+arcade.draw_text("0",95,45,arcade.color.GRAY,25)
+arcade.draw_text("-",362.5,360,arcade.color.GRAY,25)
+arcade.draw_text("+",357.5,255,arcade.color.GRAY,25)
+arcade.draw_text("enter",365,15,arcade.color.GRAY,15)
 arcade.finish_render()
 arcade.run()
 
