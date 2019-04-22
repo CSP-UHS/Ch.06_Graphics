2f35c8f7a7dbb8c58c033f0d799e9990b1edace2
aceirino <aidankugley@gmail.com>
2019-04-22 12:18:46 -0500

    take home test started 3

--- 7.0_Take_Home_Test.py
+++ 7.0_Take_Home_Test.py
@@ -14,7 +14,12 @@ for x_offset in range(0,501,20):
     arcade.draw_line(x_offset,0,x_offset,400,arcade.color.BLACK)
 for y_offset in range(0,401,20):
     arcade.draw_line(0,y_offset,500,y_offset,arcade.color.BLACK)
-arcade.draw_rectangle_filled(20,380,60,20,arcade.color.PINK)
-
+arcade.draw_rectangle_filled(50,370,60,20,arcade.color.PHLOX)
+arcade.draw_rectangle_filled(200,260,40,20,arcade.color.BLUSH,45)
+arcade.draw_circle_filled(250,200,40,arcade.color.WISTERIA)
+arcade.draw_ellipse_filled(100,100,60,20, arcade.color.AMBER)
+arcade.draw_line(80,20,120,60, arcade.color.BLUE, 1)
+arcade.draw_text("I love you. I know.",20,160,arcade.color.BRICK_RED,20)
+arcade.draw_arc_filled(400,320,50,50,ahfjkhdsfjkh.sdfhjkasdjkhflasljhkdfsalhdfs)
 arcade.finish_render()
 arcade.run()
