c90882f087b828f87993a178d25a7a622a914bd7
noahmarble <mablenoa@urbandaleschools.com>
2019-04-26 12:16:44 -0500

    take hometest done

--- 7.0_Take_Home_Test.py
+++ 7.0_Take_Home_Test.py
@@ -19,8 +19,12 @@ for i in range(20):                                                             
     arcade.draw_line(0, lineyincrement, 500, lineyincrement, arcade.color.BLACK, 1)
     lineyincrement+= 20
 arcade.draw_line(80, 20, 120, 60, arcade.color.BLUE, 1)                                 #blue line
-arcade.draw_rectangle_filled(460, 10, 5, 5, arcade.color.RED)
-arcade.draw_ellipse_filled(100, 100, 60, 20, arcade.color.AMBER)
-
+arcade.draw_rectangle_filled(460, 10, 5, 5, arcade.color.RED)                                #square
+arcade.draw_ellipse_filled(100, 100, 60, 20, arcade.color.AMBER)                            #ellipse
+arcade.draw_text("I love you. I know.", 20, 160, arcade.color.BLUSH, 20)                    #text
+arcade.draw_circle_filled(250, 200, 40, arcade.color.WISTERIA)                             #circle
+arcade.draw_lrtb_rectangle_filled(20, 80, 380, 360, arcade.color.PHLOX)                     #rectangle
+arcade.draw_rectangle_filled(200, 260, 40, 20, arcade.color.BLUSH, 45)                      #rotated rectangle
+arcade.draw_arc_filled(400, 320, 60, 60, arcade.color.YELLOW, 30, 330, 0)
 arcade.finish_render()
 arcade.run()
