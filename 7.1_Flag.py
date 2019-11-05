8a6aa428c88cb3d6484a23cd34a25903a236e901
leavertonnel <leavertonnel@urbandaleschools.com>
2019-11-05 14:06:13 -0600

    just a commit

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -10,7 +10,8 @@ We will have a competition to see who can make this flag in the least lines of c
 The record is 16! You will have to use some loops to achieve this.
 '''
 import arcade
-y = 0
+y = 10
+x = 30
 arcade.open_window(494,260, "The Stars and Stripes")
 
 arcade.set_background_color(arcade.color.WHITE) #set backround color
@@ -20,10 +21,12 @@ arcade.start_render()
 # for i in range(25):
 #     arcade.draw_rectangle_filled(y, 0, 10, 20, arcade.color.RED)
 #     y+=20
-for i in range(25):
+for i in range(15):
     arcade.draw_rectangle_filled(300, y, 600, 19.994, arcade.color.RED)
-    # arcade.draw_rectangle_filled(300, y, 600, 19.994, arcade.color.WHITE)
+    # arcade.draw_rectangle_filled(300, y, 600, 19.994, arcade.color.BLUE)
     y+=20
+arcade.draw_rectangle_filled(300, x, 600, 19.994, arcade.color.WHITE)
+
 #draw in here
 arcade.finish_render()
 
