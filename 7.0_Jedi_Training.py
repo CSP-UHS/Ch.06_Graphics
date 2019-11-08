ed12df7d77d4f4047c1e2bca75e2097ccb5b4f3e
halsteaddan <halsteaddan@urbandaleschools.com>
2019-11-08 14:20:27 -0600

    Jedi Training.

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -1,7 +1,9 @@
-#Sign your name:________________
+#Sign your name: Danny Halstead
 
 '''
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
+
+Yeet---------------
