cfaf0dd9d6d5598025b6834695ede48c876bb74b
halsteaddan <halsteaddan@urbandaleschools.com>
2019-11-11 14:25:03 -0600

    Arcade Basics.

--- Testing.py
+++ Testing.py
@@ -1,1 +1,6 @@
 import arcade
+arcade.open_window(600,600, "Revenge of the Wild Yeet") #width height title
+arcade.start_render()
+arcade.set_background_color(arcade.color.COTTON_CANDY)
+arcade.finish_render()
+arcade.run()
