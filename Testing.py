050b5efda348ec64d186bd7e0a3b0256e8823919
halsteaddan <halsteaddan@urbandaleschools.com>
2019-11-14 13:18:25 -0600

    Chapter 7 stuff.

--- Testing.py
+++ Testing.py
@@ -3,7 +3,7 @@ arcade.open_window(600,600, "Revenge of the Wild Yeet") #width height title
 arcade.set_background_color(arcade.color.COTTON_CANDY)
 arcade.start_render()
 
-
+#add stuff here
 
 arcade.finish_render()
 arcade.run()
