a1f1480a42bebe5e4cda2de7e6b116f5a5fd885e
smithdyl <smithdyl@urbandaleschools.com>
2019-11-14 13:37:07 -0600

    Picasso Finished and Fixed

--- 7.2_Picasso.py
+++ 7.2_Picasso.py
@@ -14,7 +14,7 @@ Then add a comment at the top telling the reader what you are drawing.
 IN THE WINDOW TITLE PLEASE PUT YOUR NAME.
 When you are finished Pull Request your file to your instructor.
 '''
-import arcade;arcade.open_window(500,500,"Dylan Smith");l=0
+import arcade;arcade.open_window(500,500,"Dylan Smith");i=0;offset=25;T=0
 arcade.set_background_color(arcade.color.BLACK);arcade.start_render()
 #Shoulders
 arcade.draw_arc_filled(250,0,250,175,(0,128,255),0,180)
@@ -22,11 +22,13 @@ arcade.draw_line(75,60,75,0,arcade.color.BLACK);arcade.draw_line(425,60,425,0,ar
 #Skull Base
 arcade.draw_circle_filled(250,320,130,arcade.color.WHITE);arcade.draw_circle_filled(250,245,125,arcade.color.WHITE)
 #Eyeholes
-arcade.draw_ellipse_filled(180,320,40,30,arcade.color.BLACK);arcade.draw_ellipse_filled(320,320,40,30,arcade.color.BLACK)
+for x_offset in range(100,350,140):
+    arcade.draw_ellipse_filled(80+x_offset,320,40,30,arcade.color.BLACK)
 #Iris'
 arcade.draw_circle_filled(180,320,20,arcade.color.WHITE);arcade.draw_circle_filled(320,320,20,(102,255,255))
 #Pupil's
-arcade.draw_circle_filled(180,320,8,arcade.color.BLACK);arcade.draw_circle_filled(320,320,8,arcade.color.BLACK)
+for x_offset in range(100,350,140):
+    arcade.draw_circle_filled(80+x_offset,320,8,arcade.color.BLACK)
 #Eye detail
 arcade.draw_text("*",325,300,arcade.color.WHITE,40)
 #Nose
@@ -36,8 +38,17 @@ arcade.draw_arc_outline(250,230,70,60,arcade.color.BLACK,180,360,8);arcade.draw_
 #Cheek Bones
 arcade.draw_arc_filled(170,235,20,20,arcade.color.WHITE,270,360);arcade.draw_arc_filled(330,235,20,20,arcade.color.WHITE,180,270)
 #Teeth
-arcade.draw_line(200,222,200,190,arcade.color.BLACK);arcade.draw_line(225,222,225,170,arcade.color.BLACK)
-arcade.draw_line(250,222,250,170,arcade.color.BLACK);arcade.draw_line(275,222,275,170,arcade.color.BLACK)
-arcade.draw_line(300,222,300,190,arcade.color.BLACK)
+for x_offset in range(170,300,20):
+    if T >-1 and T<3:
+        arcade.draw_line(20+x_offset,222,20+x_offset,200-i,arcade.color.BLACK)
+        i+=15
+        T+=1
+    elif T >=3:
+        arcade.draw_line(20 + x_offset, 222, 20 + x_offset, 215-i, arcade.color.BLACK)
+        i-=7.5
+        T+=1
+
+
+
 
 arcade.finish_render();arcade.run()
