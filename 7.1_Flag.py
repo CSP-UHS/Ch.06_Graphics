bd6d9e88ae5056c7b827738f638ae1b7bcdfde4c
leavertonnel <leavertonnel@urbandaleschools.com>
2019-11-14 14:31:36 -0600

    just a commit

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -11,9 +11,13 @@ The record is 16! You will have to use some loops to achieve this.
 '''
 import arcade
 y = 19.994
-z = 16
-
-
+z = 11
+l = 25
+n = 11
+m = 25
+w = 11
+b = 25
+c = 11
 arcade.open_window(494,260, "The Stars and Stripes")
 arcade.set_background_color(arcade.color.WHITE)
 arcade.start_render()
@@ -23,11 +27,36 @@ for i in range(7):
 arcade.draw_lrtb_rectangle_filled(0, 197.6, 260, 120, arcade.color.BLUE)
 
 
-
-
 for i in range(6):
-    arcade.draw_text("*", z, 125, arcade.color.WHITE,20)
+    arcade.draw_text("*", z, 230, arcade.color.WHITE,20)
     z+=32
+
+for i in range(5):
+    arcade.draw_text("*", l, 215, arcade.color.WHITE,20)
+    l+=32
+###################################
+###################################  after first 2
+# for i in range(6):
+#     arcade.draw_text("*", n, 200, arcade.color.WHITE,20)
+#     n+=32
+#
+# for i in range(5):
+#     arcade.draw_text("*", m, 185, arcade.color.WHITE,20)
+#     m+=32
+#
+# for i in range(6):
+#     arcade.draw_text("*", w, 170, arcade.color.WHITE,20)
+#     w+=32
+#
+# for i in range(5):
+#     arcade.draw_text("*", b, 155, arcade.color.WHITE,20)
+#     b+=32
+#
+# for i in range(6):
+#     arcade.draw_text("*", c, 140, arcade.color.WHITE,20)
+#     c+=32
+
+
 
 arcade.finish_render()
 arcade.run()
