c796004731d7c6adb8e691464a21a01b85577580
halsteaddan <halsteaddan@urbandaleschools.com>
2019-11-14 13:12:33 -0600

    6.0 Jedi Fixed.

--- Testing.py
+++ Testing.py
@@ -2,5 +2,8 @@ import arcade
 arcade.open_window(600,600, "Revenge of the Wild Yeet") #width height title
 arcade.set_background_color(arcade.color.COTTON_CANDY)
 arcade.start_render()
+
+
+
 arcade.finish_render()
 arcade.run()
