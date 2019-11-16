adc0a51964be32739348f7e3f06bd3a0f4259932
leavertonnel <leavertonnel@urbandaleschools.com>
2019-11-16 12:44:11 -0600

    just a commit

--- captian_marvel.py
+++ captian_marvel.py
@@ -1,20 +1,38 @@
 import arcade
 
 arcade.open_window(700,400, "Nellie Leaverton")
-
-arcade.set_background_color(arcade.color.WHITE) #set backround color
-
 arcade.start_render()
 ###############
 arcade.draw_lrtb_rectangle_filled(0,700,400,240, arcade.color.BOSTON_UNIVERSITY_RED) #TOP RED
 arcade.draw_lrtb_rectangle_filled(0, 700, 240, 0, arcade.color.BLUEBONNET) #BOTTOM BLUE
-### #1 GREEN
+### #1 YELLOW
+a = 279
+b = 303
+c = 270
+d = 249
+
+my_list=()
+###### for the loop
+# for i in range(2):
+#     my_list = (
+#         (0,a),
+#         (120, b),
+#         (120, c),
+#         (0, d)
+#         )
+#     arcade.draw_polygon_filled(my_list, arcade.color.UROBILIN)
+#     a = a - 20
+#     b = b - 20
+#     c = c - 20
+#     d = d - 20
+############
+
 my_list = (
-    (0,279),
-    (120, 303),
-    (120, 270),
-    (0, 249)
-)
+    (0,a),
+    (120, b),
+    (120, c),
+    (0, d)
+    )
 arcade.draw_polygon_filled(my_list, arcade.color.UROBILIN)
 ### #2
 my_list = (
