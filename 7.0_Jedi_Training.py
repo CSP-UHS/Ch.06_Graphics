e435f8a88800556cec1c4045161de4124a0aad73
lilyb36 <burkheadlil@urbandaleschools.com>
2019-11-17 14:22:56 -0600

    getting repo to work from home

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -5,3 +5,19 @@ Recreate, exactly the Test Picture from the website. The arcade colors used in t
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
+
+import arcade
+arcade.open_window(500, 400, "Ch.7 Jedi Training")
+arcade.set_background_color(arcade.color.ALMOND)
+arcade.start_render()
+x_offset=0
+for i in range(25):
+    arcade.draw_line(x_offset,0,x_offset,400,arcade.color.BLACK)
+    x_offset+=20
+y_offset=0
+for i in range(20):
+    arcade.draw_line(0,y_offset,500,y_offset,arcade.color.BLACK)
+    y_offset+=20
+arcade.draw_rectangle_filled(40,480,60,25,arcade.color.)
+arcade.finish_render()
+arcade.run()
