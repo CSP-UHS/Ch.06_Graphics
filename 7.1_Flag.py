a75513094e274966b95fab686672300e2c3ab1df
halsteaddan <halsteaddan@urbandaleschools.com>
2019-11-18 14:21:31 -0600

    Flag.

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -10,17 +10,21 @@ We will have a competition to see who can make this flag in the least lines of c
 The record is 16! You will have to use some loops to achieve this.
 '''
 
-import arcade
+import arcade #Setup and White Background
 arcade.open_window(494,260, "The Stars and Stripes")
 arcade.set_background_color(arcade.color.WHITE)
 arcade.start_render()
 
-for y in range(10,251,40):
+for y in range(10,251,40): #Red Stripes
     arcade.draw_rectangle_filled(247, y, 494,20, (191, 10, 48))
 
+arcade.draw_rectangle_filled(99, 190, 198, 140, (0, 40, 104)) #Top Left Blue Box
 
+TextHeight=  #White Stars
+for StarRows in range(9):
+    if StarRows%2==0:
+        arcade.draw_text("*  *  *  *  *  *", 15, TextHeight, arcade.color.RADICAL_RED)
 
-arcade.finish_render()
+arcade.finish_render() #More Arcade Setup
 arcade.run()
 
-#260 up 494 aisw
