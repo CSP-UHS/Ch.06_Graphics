848e91c441934c9e20345c275ba98c885f8869b4
hermoniuscapensis <phamjul@urbandaleschools.com>
2019-11-19 14:16:44 -0600

    Just a commit

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -19,10 +19,24 @@ for y in range(10,251,40):
     arcade.draw_rectangle_filled(247,y,494,20,(191,10,48))
 
 arcade.draw_rectangle_filled(99,190,198,140,(0, 40, 104))
-y=245
+y=235
 for star_rows in range(9):
     if star_rows%2==0:
-        arcade.draw_text("*  *  *  *  *  *",15,y,arcade.color.WHITE,20)
-    y-=13
+        x=8
+        for star in range(6):
+            if star==9:
+                arcade.draw_text("*",x,y,arcade.color.WHITE,20)
+            else:
+                arcade.draw_text("*",x,y,arcade.color.WHITE,20)
+                x+=35
+    else:
+        x=25
+        for star in range(5):
+            if star==9:
+                arcade.draw_text("*",x,y,arcade.color.WHITE,20)
+            else:
+                arcade.draw_text("*",x,y,arcade.color.WHITE,20)
+                x+=35
+    y-=15
 arcade.finish_render()
 arcade.run()
