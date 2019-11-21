378e2104e9feceefb0cee07c05791715ff1e6965
halsteaddan <halsteaddan@urbandaleschools.com>
2019-11-21 14:09:19 -0600

    Jedi Training.

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -6,12 +6,22 @@ BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
 
-import arcade
+import arcade #Setup Arcade
 arcade.open_window(500,400, "These Aren’t The Droids You’re Looking For...")
-arcade.set_background_color(arcade.color.WHITE)
+arcade.set_background_color(arcade.color.ALMOND)
 arcade.start_render()
 
-#Stuff
+YValue = 400 #Up and Down Lines
+for z in range(22):
+    arcade.draw_line(0, YValue, 500, YValue, arcade.color.BLACK, 1)
+    YValue -= 19.04
 
-arcade.finish_render()
+XValue = 0 #Left and Right Lines
+for z in range(27):
+    arcade.draw_line(XValue, 400, XValue, 0, arcade.color.BLACK, 1)
+    XValue += 19.23
+
+Yeet
+
+arcade.finish_render() #Run Arcade
 arcade.run()
