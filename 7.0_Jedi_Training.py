cb6f9437a6d28cc6e7ad4c793efed9da3f4eb4ec
hmarmal <hmarmal@urbandaleschools.com>
2019-11-21 13:57:53 -0600

    jeditraining 7

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -1,7 +1,31 @@
-#Sign your name:________________
+#Sign your name:  Malsawmthara Hmar (Henry)
 
 '''
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
+
+import arcade
+
+arcade.open_window(500,400,"Jedi Training")
+arcade.set_background_color(arcade.color.ALMOND)
+arcade.start_render()
+x_offset=0
+y_offset=0
+for i in range(25):
+    x_offset+=20
+    arcade.draw_line(x_offset,0,x_offset,400,arcade.color.BLACK)  #draw the vertical lines
+for i in range(20):
+    y_offset+=20
+    arcade.draw_line(0,y_offset,500,y_offset,arcade.color.BLACK)  #draw the horizontal lines
+arcade.draw_line(80,20,120,60,arcade.color.BLUE) #draw the blue line
+arcade.draw_ellipse_filled(100,100,60,20,arcade.color.AMBER)    # drawing the amber oval at the bottom
+arcade.draw_rectangle_filled(50,370,60,20,arcade.color.PHLOX)   # draw the pink rectangle
+arcade.draw_rectangle_filled(200,260,40,20,arcade.color.BLUSH,45) # rotated rectangle
+arcade.draw_circle_filled(250,200,40,arcade.color.WISTERIA) # circle in the middle
+arcade.draw_text("I love you. I know.", 21, 160, arcade.color.BRICK_RED, 20) # the text
+arcade.draw_rectangle_filled(460,10,5,5,arcade.color.RED) #little red dot
+arcade.draw_arc_filled(400,320,60,60,arcade.color.YELLOW,29,330,0) # yellow arc
+arcade.finish_render()
+arcade.run()
--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -3,9 +3,41 @@ FLAG PROJECT
 ---------------
 Make your flag 260 pixels tall
 Use the scaling image on the website to determine other dimensions
-The hexadecimal colors for the official flag are red:#BF0A30 and blue:#002868
+The hexadecimal colors for the official flag are red:#BF0A30(191,10,48) and blue:#002868(0,40,104)
 Title the window, "The Stars and Stripes"
 I used a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
 '''
+
+import arcade
+
+arcade.open_window(494,260,"The Stars and Stripes")
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+for y in range(10,251,40):
+    arcade.draw_rectangle_filled(247,y,494,20,(191,10,48))
+arcade.draw_rectangle_filled(99,190,197.6,140,(0,40,104))
+# draw stars
+y=235
+for star_rows in range(9):
+    if star_rows%2==0:
+        x=6
+        for star in range(6):
+            if star==0:
+                arcade.draw_text("*",x,y,arcade.color.WHITE,20)
+            else:
+                arcade.draw_text("*",x,y,arcade.color.WHITE,20)
+            x+=35
+    else:
+        x = 23
+        for star in range(5):
+            if star == 0:
+                arcade.draw_text("*", x, y, arcade.color.WHITE, 20)
+            else:
+                arcade.draw_text("*", x, y, arcade.color.WHITE, 20)
+            x += 35
+    y-=15
+
+arcade.finish_render()
+arcade.run()
