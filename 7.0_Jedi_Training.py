f3bc0895408cef85255f40a62250d125f1dbb9bc
halsteaddan <halsteaddan@urbandaleschools.com>
2019-11-21 13:29:54 -0600

    Jedi Training.

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -6,4 +6,12 @@ BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
 
+import arcade
+arcade.open_window(500,400, "These Aren’t The Droids You’re Looking For...")
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
 
+#Stuff
+
+arcade.finish_render()
+arcade.run()
