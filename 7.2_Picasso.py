2b21a7080b67897f599aed86ac1c4676b39edd2d
buiste <buiste@urbandaleschools.com>
2019-12-05 13:54:40 -0600

    commit

--- 7.2_Picasso.py
+++ 7.2_Picasso.py
@@ -14,7 +14,21 @@ Then add a comment at the top telling the reader what you are drawing.
 IN THE WINDOW TITLE PLEASE PUT YOUR NAME.
 When you are finished Pull Request your file to your instructor.
 '''
-
-
+import arcade
+arcade.open_window(300,300,"steve")
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+arcade.draw_ellipse_filled(150,150,110,70,(0,128,0))
+arcade.draw_circle_filled(215,200,40,(0,128,0))
+arcade.draw_circle_filled(80,200,40,(0,128,0))
+arcade.draw_circle_filled(80,200,20,arcade.color.WHITE)
+arcade.draw_circle_filled(215,200,20,arcade.color.WHITE)
+arcade.draw_circle_filled(80,200,15,arcade.color.BLACK)
+arcade.draw_circle_filled(215,200,15,arcade.color.BLACK)
+arcade.draw_ellipse_filled(150,130,80,20,arcade.color.PINK)
+arcade.draw_ellipse_filled(150,130,40,10,arcade.color.CANDY_PINK)
+arcade.draw_text("Croak",20,60,arcade.color.DARK_GREEN,12,)
+arcade.finish_render()
+arcade.run()
 
 
