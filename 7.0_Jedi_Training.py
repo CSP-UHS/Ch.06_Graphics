35dfca9cd608d1ba40b2d67abe1f7b1fd971ed7e
alexanderrow <alexanderrow@urbandaleschools.com>
2019-12-05 14:08:23 -0600

    just need to do pac man

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -20,6 +20,11 @@ for z in range(25):
 for z in range(25):
     arcade.draw_line(500,h,0,h,arcade.color.BLACK)
     h+=20
-
+arcade.draw_xywh_rectangle_filled(20,360,60,20,arcade.color.PHLOX)
+arcade.draw_rectangle_filled(200,280,40,20,arcade.color.BLUSH,45)
+arcade.draw_circle_filled(250,220,40,arcade.color.WISTERIA)
+arcade.draw_line(80,20,120,60,arcade.color.BLUE,1)
+arcade.draw_ellipse_filled(100,100,60,20,arcade.color.AMBER,0,128)
+arcade.draw_text("I love you. I know.",20,180,arcade.color.BRICK_RED,20,220,"left","Calibri",False,False,"left","baseline",0)
 arcade.finish_render()
 arcade.run()
---
+++ scratch.py
@@ -0,0 +1,2 @@
+grade = 35.92921
+print(f"your grade is {grade:.2f}")
