75bfdf04fa82aea0d90e66638508758c20ca7d0b
trannha <trannha@urbandaleschools.com>
2019-12-13 14:16:06 -0600

    wowowo

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -17,10 +17,28 @@ arcade.start_render()
 for y in range (10,251,40):
     arcade.draw_rectangle_filled(247,y,494,20,(191,10,48))
 arcade.draw_rectangle_filled(99,190,198,140,(0,40,104))
+
+y=235
 for star_rows in range(9):   
     if star_rows%2==0:
-        arcade.draw_text("* * * * * *",15,y,arcade.color.WHITE,20)
+        x=8
+        for star in range(6):
+            if star==0:
+                arcade.draw_text("*",x,y,arcade.color.WHITE,20)
+            else:
+                arcade.draw_text("*", x, y, arcade.color.WHITE, 20)
+            x+=35
     else:
-        arcade.draw_text("  *  *  *  *  *  *",32,y,arcade.color.white,20)
+        x=25
+        for star in range(6):
+            if star == 0:
+                arcade.draw_text("*", x, y, arcade.color.WHITE, 20)
+            else:
+                arcade.draw_text("*", x, y, arcade.color.WHITE, 20)
+                x+=35
+
+    y-=15
+
+
 arcade.finish_render()
 arcade.run()
