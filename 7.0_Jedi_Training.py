ddd4dea13ebf47762e97859011301330860a35a6
halsteaddan <halsteaddan@urbandaleschools.com>
2019-12-19 13:57:31 -0600

    Finished Jedi Training.

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -21,13 +21,25 @@ for z in range(27):
     arcade.draw_line(XValue, 400, XValue, 0, arcade.color.BLACK, 1)
     XValue += 19.23
 
+
+
 arcade.draw_xywh_rectangle_filled(20,362,58,18,arcade.color.PHLOX) #Top left purple rectangle
 
-arcade.draw_arc_filled(400,325,60,60,arcade.color.YELLOW,30,330,0) #Pacman
+arcade.draw_arc_filled(405,320,60,60,arcade.color.YELLOW,30,330,0) #Pacman
 
 arcade.draw_rectangle_filled(196,265,40,20,arcade.color.BLUSH,45) #Pink angled rectangle
 
+arcade.draw_circle_filled(250,211,40,arcade.color.WISTERIA) #Middle Circle light purple
+
+arcade.draw_text("I live you.I know.",20,170,arcade.color.BRICK_RED,20,220,"left","Calibri", False,False,"left","baseline" ,0) #Textbox
+
 arcade.draw_ellipse_filled(98,96,60,20,arcade.color.AMBER,0,128) #Bottom left elipse
+
+arcade.draw_line(77,17,118,58,arcade.color.BLUE,1)
+
+arcade.draw_rectangle_filled(462,10,5,5,arcade.color.RED,0) #Bottom right small rectangle
+
+
 
 arcade.finish_render() #Run Arcade
 arcade.run()
