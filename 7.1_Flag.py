057d100342af64386f449df046c66ce20f43af42
catherineuhs <cliffordcat@urbandaleschools.com>
2020-01-05 19:19:57 -0600

    :^)pizza

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -9,3 +9,5 @@ I used a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
 '''
+
+
--- 7.2_Picasso.py
+++ 7.2_Picasso.py
@@ -18,3 +18,54 @@ When you are finished Pull Request your file to your instructor.
 
 
 
+import arcade
+arcade.open_window(500,400,"Jedi Training")
+arcade.set_background_color(arcade.color.CHESTNUT)
+arcade.start_render()
+
+dot_list = ((340, 80),
+              (100, 200),
+              (360, 380),
+            (340, 80))
+arcade.draw_polygon_filled(dot_list, arcade.color.CYBER_YELLOW)
+
+
+point_list = ((320, 20),
+              (80, 160),
+              (120, 200),
+              (341, 78),
+              (320, 20))
+arcade.draw_polygon_filled(point_list, arcade.color.CHOCOLATE)
+#
+arcade.draw_circle_filled(100, 180, 27, arcade.color.CHOCOLATE)
+arcade.draw_circle_filled(205, 200, 30, arcade.color.FIREBRICK)
+arcade.draw_circle_filled(200, 200, 30, arcade.color.FIRE_ENGINE_RED)
+arcade.draw_circle_filled(305, 280, 30, arcade.color.FIREBRICK)
+arcade.draw_circle_filled(300, 280, 30, arcade.color.FIRE_ENGINE_RED)
+arcade.draw_circle_filled(305, 160, 30, arcade.color.FIREBRICK)
+arcade.draw_circle_filled(300, 160, 30, arcade.color.FIRE_ENGINE_RED)
+
+for x_offset in range(0,400,20):
+    arcade.draw_text("pizza", 10, 0+x_offset, arcade.color.CHAMPAGNE, 20)
+
+
+
+arcade.draw_arc_filled(330.5, 49, 27, 27,
+                       arcade.color.CHROME_YELLOW, 73, 255, 1)
+
+
+
+arcade.finish_render()
+arcade.run()
+
+
+
+
+
+arcade.finish_render()
+arcade.run()
+
+
+
+
+
