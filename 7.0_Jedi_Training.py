5e67a177a23bf18be1acb7650da0eea6122fa03d
waltersjac <waltersjac@urbandaleschools.com>
2020-12-07 10:11:10 -0600

    1st test commit

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -5,3 +5,17 @@ Recreate, exactly the Test Picture from the website. The arcade colors used in t
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
+import arcade
+arcade.open_window(500,400,"Drawling")
+arcade.set_background_color(arcade.color.ALMOND)
+arcade.start_render()
+
+
+arcade.draw_rectangle_filled(50,370,60,20,arcade.color.PHLOX)
+
+arcade.draw_line(80,20,120,60,arcade.color.BLUE)
+arcade.draw_text("I love you. I know",20,160,arcade.color.BLUSH,12)
+
+
+arcade.finish_render()
+arcade.run()
--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -9,3 +9,19 @@ I used a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
 '''
+import arcade
+arcade.open_window(494,260,"The Stars and Stripes")
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+
+for xoffset in range(10,520,40):
+    arcade.draw_rectangle_filled(247,0+xoffset,494,20,(191,11,48))
+
+arcade.draw_rectangle_filled(98.8,190,197.6,140,(0,40,104))
+
+arcade.draw_polygon_filled()
+
+
+
+arcade.finish_render()
+arcade.run()
