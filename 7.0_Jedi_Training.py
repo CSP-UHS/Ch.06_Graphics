401b5e37a634ec722e488af912e35c23a3a25d35
neyensian <neyensian@urbandaleschools.com>
2020-12-14 10:01:37 -0600

    asdf

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -1,7 +1,28 @@
-#Sign your name:________________
+#Sign your name:Ian Neyens
 
 '''
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
+
+import arcade
+arcade.open_window(500, 400, "Jedi Training")
+arcade.set_background_color(arcade.color.ALMOND)
+arcade.start_render()
+
+for vert_line in range(20, 500, 20):
+    arcade.draw_line(vert_line, 0, vert_line, 500, arcade.color.BLACK)
+for horiz_line in range(20, 500, 20):
+    arcade.draw_line(0, horiz_line, 500, horiz_line, arcade.color.BLACK)
+arcade.draw_lrtb_rectangle_filled(20, 80, 380, 360, arcade.color.PHLOX)
+arcade.draw_line(80, 20, 120, 60, arcade.color.BLUE)
+arcade.draw_ellipse_filled(100, 100, 120, 40, arcade.color.AMBER)
+arcade.draw_circle_filled(250, 200, 40, arcade.color.WISTERIA)
+arcade.draw_rectangle_filled(200, 260, 20, 40, arcade.color.BLUSH, 45)
+arcade.draw_arc_filled(400, 320, 120, 120, arcade.color.YELLOW, 30, 330, 0)
+arcade.draw_rectangle_filled(460, 10, 5, 5, arcade.color.RED)
+arcade.draw_text("I love you. I know.", 20, 155, arcade.color.BRICK_RED, 21)
+
+arcade.finish_render()
+arcade.run()
--- 7.2_Pycasso.py
+++ 7.2_Pycasso.py
@@ -15,6 +15,49 @@ IN THE WINDOW TITLE PLEASE PUT YOUR NAME.
 When you are finished Pull Request your file to your instructor.
 '''
 
+import arcade
+arcade.open_window(500, 500, "Ian N")
+arcade.set_background_color(arcade.color.SKY_BLUE)
+arcade.start_render()
 
+arcade.draw_circle_filled(475, 475, 50, arcade.color.SUNGLOW)       #Sun
 
+arcade.draw_circle_filled(50, 450, 25, arcade.color.WHITE)      #Cloud 1
+arcade.draw_circle_filled(70, 460, 25, arcade.color.WHITE)
+arcade.draw_circle_filled(80, 450, 25, arcade.color.WHITE)
 
+arcade.draw_circle_filled(175, 470, 25, arcade.color.WHITE)         #Cloud 2
+arcade.draw_circle_filled(190, 450, 25, arcade.color.WHITE)
+arcade.draw_circle_filled(210, 470, 25, arcade.color.WHITE)
+
+arcade.draw_circle_filled(375, 400, 25, arcade.color.WHITE)         #Cloud 3
+arcade.draw_circle_filled(350, 420, 25, arcade.color.WHITE)
+
+arcade.draw_rectangle_filled(250, 150, 500, 300, arcade.color.GREEN)        #Grass
+
+arcade.draw_rectangle_filled(250, 250, 300, 200, arcade.color.WHITE)        #House
+
+arcade.draw_triangle_filled(250, 470, 50, 340, 450, 340, arcade.color.RED)      #Roof
+
+arcade.draw_rectangle_filled(250, 212, 65, 125, arcade.color.MIDNIGHT_BLUE)     #Door
+arcade.draw_circle_filled(230, 212, 5, arcade.color.SILVER_CHALICE)
+
+arcade.draw_rectangle_outline(160, 250, 50, 50, arcade.color.BLACK)             #Window
+arcade.draw_rectangle_filled(160, 250, 49, 49, arcade.color.LIGHT_CYAN)
+arcade.draw_line(135, 250, 185, 250, arcade.color.BLACK)
+arcade.draw_line(160, 275, 160, 225, arcade.color.BLACK)
+
+arcade.draw_rectangle_outline(340, 250, 50, 50, arcade.color.BLACK)         #Window
+arcade.draw_rectangle_filled(340, 250, 49, 49, arcade.color.LIGHT_CYAN)
+arcade.draw_line(315, 250, 365, 250, arcade.color.BLACK)
+arcade.draw_line(340, 275, 340, 225, arcade.color.BLACK)
+
+arcade.draw_rectangle_filled(250, 50, 80, 200, arcade.color.GRAY)      #Path
+
+for fence in range(4, 500, 12):                                                         #Fence
+    arcade.draw_rectangle_filled(fence, 50, 10, 100, arcade.color.SADDLE_BROWN)
+arcade.draw_rectangle_filled(250, 75, 500, 10, arcade.color.SADDLE_BROWN)
+arcade.draw_rectangle_filled(250, 25, 500, 10, arcade.color.SADDLE_BROWN)
+
+arcade.finish_render()
+arcade.run()
