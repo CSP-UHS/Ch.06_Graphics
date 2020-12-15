43fa4c1d8ab0d5df2df64974ac6c855778e106c4
Geni W <williamsgen@urbandaleschools.com>
2020-12-15 01:29:51 -0600

    Pycasso sea, birds, lighthouse

--- 7.2_Pycasso.py
+++ 7.2_Pycasso.py
@@ -14,7 +14,39 @@ Then add a comment at the top telling the reader what you are drawing.
 IN THE WINDOW TITLE PLEASE PUT YOUR NAME.
 When you are finished Pull Request your file to your instructor.
 '''
+import arcade
+import random
+bird_count = 0
+arcade.open_window(800,600, "Geni Williams Pycasso")
+arcade.set_background_color((173, 216, 230))
+screen_width = 800
+screen_height = 600
+arcade.start_render()
+
+#draws sea & lighthouse
+arcade.draw_rectangle_filled(screen_width / 2, screen_height / 6, screen_width - 1, screen_height / 3, (52, 143, 171))
+arcade.draw_rectangle_filled(100, 0, 150, 800, arcade.color.ASH_GREY)
+arcade.draw_rectangle_filled(100, 100, 50, 50, arcade.color.BLACK)
+arcade.draw_rectangle_filled(100, 300, 50, 50, arcade.color.BLACK)
+arcade.draw_rectangle_filled(100, 400, 200, 50, arcade.color.ASH_GREY)
+arcade.draw_triangle_filled(30, 425, 170, 425, 105, 525, arcade.color.AMERICAN_ROSE)
+arcade.draw_circle_filled(105,525,10, (247, 244, 213))
+
+#draws boat
 
 
+#draws birds
+def draw_bird(x, y):
+    arcade.draw_arc_outline(x, y, 20, 20, arcade.color.BLACK, 0, 90)
+    arcade.draw_arc_outline(x + 20, y, 20, 20, arcade.color.BLACK, 90, 180)
+
+while bird_count < 5:
+    #random x & y on screen
+    x = random.randrange(0, screen_width)
+    y = random.randrange(screen_height / 3, screen_height - 20)
+    draw_bird(x,y)
+    bird_count += 1
 
 
+arcade.finish_render()
+arcade.run()
