e6dee10ff74b4af0b34920bdd7816ad70779398f
getzbry <getzbry@urbandaleschools.com>
2021-01-05 10:21:35 -0600

    finished

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -9,3 +9,19 @@ I used a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
 '''
+import arcade#ALWAYS USE IMPORT ARCADE
+arcade.open_window(494, 260, "The Starts and Stripes")
+arcade.set_background_color((255, 255, 255,))
+arcade.start_render()
+for y in range (250, 0, -40):
+    arcade.draw_rectangle_filled(247, y, 494, 20, (191, 10, 48))
+arcade.draw_lrtb_rectangle_filled(0, 198, 260, 120, (0, 40, 104))
+y = 230
+for stars in range (9):
+    if stars% 2 == 0:
+        arcade.draw_text("*   *   *   *   *   *", 15, y, arcade.color.WHITE, 20)
+    else:
+        arcade.draw_text("  *   *   *   *   *", 15, y, arcade.color.WHITE, 20)
+    y = y-13
+arcade.finish_render()
+arcade.run()
--- 7.2_Pycasso.py
+++ 7.2_Pycasso.py
@@ -17,4 +17,30 @@ When you are finished Pull Request your file to your instructor.
 
 
 
+import random
+import arcade
+arcade.open_window(600, 600, "Pycasso Project, Bryce Getz")
+arcade.set_background_color((0, 150, 255))
+arcade.start_render()
 
+
+
+
+arcade.draw_lrtb_rectangle_filled(200, 400, 500, 0, (0, 0, 0)) #Back drop for the building
+
+
+for y in range (0, 505, 10): #horizontal levels
+    arcade.draw_rectangle_filled(300, y, 200, 5, (255, 255, 255))
+
+for x in range (200, 405, 10):#VERTICAL LINES
+    arcade.draw_rectangle_filled(x, 250, 5, 500, (255, 255, 255))
+
+
+for y in range (0, 10, 3):
+    arcade.draw_line(0, random(-3, 3), 5)
+
+
+
+arcade.finish_render()
+
+arcade.run()
