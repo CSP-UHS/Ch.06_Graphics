f898a5ae35d6b0bff7a61436ca6a9f630c95df60
thawngbaw <thawngbaw@urbandaleschools.com>
2021-01-06 09:25:41 -0600

    jedi training

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -5,3 +5,23 @@ Recreate, exactly the Test Picture from the website. The arcade colors used in t
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
+import arcade
+arcade.open_window(500, 400, "Drawing")
+arcade.set_background_color((239, 222, 204))
+arcade.start_render()
+#Drawing commands
+for loo in range(0, 400, 20):
+    arcade.draw_line(0, 20+loo, 500, 20+loo, (0, 0, 0), 1)
+for loo in range(0, 500, 20):
+    arcade.draw_line(20+loo, 0, 20+loo, 500, (0, 0, 0), 1)
+arcade.draw_line(80, 20, 120, 60, (108, 95, 231))
+arcade.draw_rectangle_filled(460, 10, 4, 4, (237, 56, 51), 0)
+arcade.draw_rectangle_filled(50, 370, 60, 20, (229, 87, 249), 0)
+arcade.draw_rectangle_filled(200, 260, 40, 20, (225, 91, 131), 140)
+arcade.draw_circle_filled(250, 200, 40, (202, 158, 221), 1)
+arcade.draw_text("I love you. I know.", 20, 154, (240, 75, 62), 21)
+arcade.draw_ellipse_filled(100, 100, 120, 40, (248, 193, 69), 1)
+arcade.draw_arc_filled(400, 320, 120, 120, arcade.color.YELLOW, 30, 330)
+
+arcade.finish_render()
+arcade.run()
--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -9,3 +9,34 @@ I used a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
 '''
+import arcade
+arcade.open_window(494, 260, "The Stars and Stripes")
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+for loo in range(0, 260, 40):
+    arcade.draw_line(0, 9+loo, 494, 9+loo, (191, 10, 48), 20)
+arcade.draw_rectangle_filled(99, 189, 198, 140, (0, 40, 104))
+for star in range(0, 155, 25):
+    arcade.draw_text("*", 10, 119+star, (255, 255, 255), 20)
+for side in range(0, 90, 24):
+    arcade.draw_text("*", 27, 133 + side, (255, 255, 255), 20)
+for star in range(0, 155, 25):
+    arcade.draw_text("*", 44, 119 + star, (255, 255, 255), 20)
+for side in range(0, 90, 24):
+    arcade.draw_text("*", 62, 133.4 + side, (255, 255, 255), 20)
+for star in range(0, 155, 25):
+    arcade.draw_text("*", 78, 119 + star, (255, 255, 255), 20)
+for side in range(0, 90, 24):
+    arcade.draw_text("*", 95, 133.4 + side, (255, 255, 255), 20)
+for star in range(0, 155, 25):
+    arcade.draw_text("*", 113, 119 + star, (255, 255, 255), 20)
+for side in range(0, 90, 24):
+    arcade.draw_text("*", 129, 133.4 + side, (255, 255, 255), 20)
+for star in range(0, 155, 25):
+    arcade.draw_text("*", 146, 119 + star, (255, 255, 255), 20)
+for side in range(0, 90, 24):
+    arcade.draw_text("*", 163, 133.4 + side, (255, 255, 255), 20)
+for star in range(0, 155, 25):
+    arcade.draw_text("*", 180, 119 + star, (255, 255, 255), 20)
+arcade.finish_render()
+arcade.run()
