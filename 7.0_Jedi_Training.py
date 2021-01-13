3f3a2e0c95cc0abd18b0e49c16c61f5ebf5e99dd
MitchellDan <mitchelldan@urbandalescools.com>
2021-01-13 11:17:58 -0600

    did lots

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -5,3 +5,32 @@ Recreate, exactly the Test Picture from the website. The arcade colors used in t
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.\
 '''
+import arcade
+
+arcade.open_window(500,400,"Jedi Training")
+arcade.set_background_color((239, 222, 205))
+
+arcade.start_render()
+for x_off in range(0,500,20):
+    arcade.draw_line((x_off), 0, (x_off), 400, arcade.color.BLACK, 2)
+
+for y_off in range(0,400,20):
+    arcade.draw_line(0, (y_off), 500, (y_off), arcade.color.BLACK, 2)
+
+arcade.draw_lrtb_rectangle_filled(20, 80, 380, 360, (223, 0, 255))
+
+arcade.draw_line(80, 20, 120, 60, arcade.color.BLUE, 1)
+
+arcade.draw_circle_filled(250, 200, 40, arcade.color.WISTERIA)
+
+arcade.draw_ellipse_filled(100, 100, 120, 40, arcade.color.AMBER)
+
+arcade.draw_rectangle_filled(200, 260, 40, 20, arcade.color.BLUSH, 135)
+
+arcade.draw_text("I love you. I know.", 45, 155, arcade.color.BRICK_RED, 20)
+
+arcade.draw_arc_filled(320, 320, 120, 120, arcade.color.YELLOW, 340, 20, 0, 128)
+
+arcade.finish_render()
+
+arcade.run()
--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -9,3 +9,19 @@ I used a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
 '''
+import arcade
+y_off = 0
+arcade.open_window(494,260,"The Stars and Stripes")
+arcade.set_background_color((191, 10, 48))
+arcade.start_render()
+
+for y_off in range(0,220,40):
+    arcade.draw_lrtb_rectangle_filled(0, 494, (240 - y_off), (220 - y_off), arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(0, 198, 260, 140, (0, 40, 104))
+for i in range(0,6,1):
+    arcade.draw_text("*", 16, 100, arcade.color.WHITE, 20)
+
+
+arcade.finish_render()
+
+arcade.run()
