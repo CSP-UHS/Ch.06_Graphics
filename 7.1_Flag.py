5e19a703789048958114dd904f418b313846e3a8
MitchellDan <mitchelldan@urbandalescools.com>
2021-01-13 19:23:47 -0600

    did flag

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -10,18 +10,21 @@ We will have a competition to see who can make this flag in the least lines of c
 The record is 16! You will have to use some loops to achieve this.
 '''
 import arcade
-y_off = 0
 arcade.open_window(494,260,"The Stars and Stripes")
 arcade.set_background_color((191, 10, 48))
 arcade.start_render()
-
-for y_off in range(0,220,40):
-    arcade.draw_lrtb_rectangle_filled(0, 494, (240 - y_off), (220 - y_off), arcade.color.WHITE)
+for l_off in range(0,220,40):
+    arcade.draw_lrtb_rectangle_filled(0, 494, (240 - l_off), (220 - l_off), arcade.color.WHITE)
 arcade.draw_lrtb_rectangle_filled(0, 198, 260, 140, (0, 40, 104))
-for i in range(0,6,1):
-    arcade.draw_text("*", 16, 100, arcade.color.WHITE, 20)
-
-
+for i in range(0,5,1):
+    for t_off in range (0,17,16):
+        for s1_off in range(226,120,-23):
+            for s2_off in range(16,179,32):
+                if ((t_off)+(s2_off)) > 180:
+                    break
+                elif ((s1_off)-(t_off)) < 120:
+                    break
+                else:
+                    arcade.draw_text("*", ((s2_off)+(t_off)), ((s1_off)-((t_off)-0.3*(t_off))), arcade.color.WHITE, 20)
 arcade.finish_render()
-
 arcade.run()
