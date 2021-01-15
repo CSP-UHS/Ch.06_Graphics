78af99109bff677aa48dec1ae825493135f4d3c3
thonathan <januszewskieth@urbandaleschools.com>
2021-01-15 12:57:12 -0600

    ch7 stuff

--- 7.2_Pycasso.py
+++ 7.2_Pycasso.py
@@ -14,7 +14,29 @@ Then add a comment at the top telling the reader what you are drawing.
 IN THE WINDOW TITLE PLEASE PUT YOUR NAME.
 When you are finished Pull Request your file to your instructor.
 '''
+import arcade
 
+arcade.open_window(1000,600, "Ethan Januszewski")
+arcade.set_background_color(arcade.color.ARSENIC)
 
+arcade.start_render()
 
+for i in range(1000):
+    arcade.draw_line(0, i*10, 1000, i*10, arcade.color.BLACK, 2)
+    arcade.draw_line(i*10,0 , i*10,1000 , arcade.color.BLACK, 2)
 
+arcade.draw_circle_filled(500,300,325,arcade.color.CHARCOAL)
+
+arcade.draw_line(450,200,600,200,arcade.color.AERO_BLUE,3)
+arcade.draw_line(600,200,500,425,arcade.color.AERO_BLUE,3)
+arcade.draw_line(500,425,375,100,arcade.color.AERO_BLUE,3)
+arcade.draw_line(375,100,250,100,arcade.color.AERO_BLUE,3)
+arcade.draw_line(250,100,425,550,arcade.color.AERO_BLUE,3)
+arcade.draw_line(425,550,575,550,arcade.color.AERO_BLUE,3)
+arcade.draw_line(575,550,750,100,arcade.color.AERO_BLUE,3)
+arcade.draw_line(750,100,525,100,arcade.color.AERO_BLUE,3)
+arcade.draw_line(525,100,450,200,arcade.color.AERO_BLUE,3)
+
+arcade.finish_render()
+
+arcade.run()
